b841f54b146679a5e0905c285fd8f94df4aeb806
Mohammad Ismail <mohammadn.ismail@mail.utoronto.ca>
2017-11-21 03:52:18 -0500

    Removed old website files, brought out public folder contents to root of website folder for covnenience

--- shAReHackWestern4-website/assets/js/dashboard_auth.js
+++ shAReHackWestern4-website/assets/js/dashboard_auth.js
@@ -1,23 +1,3 @@
-// Asynchronously iniitializes Facebook Integration
-// App ID, API version, xfbml = true
-window.fbAsyncInit = function() {
-    FB.init({
-        appId      : '291168591401232',
-        xfbml      : true,
-        version    : 'v2.11'
-    });
-    FB.AppEvents.logPageView();
-};
-
-// Load facebook SDK Asynchronously
-function(d, s, id){
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {return;}
-    js = d.createElement(s); js.id = id;
-    js.src = "https://connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk');
-
 /**
  * On-click callback for facebook login button
  */
@@ -34,10 +14,12 @@ function checkLoginState() {
  */
 function connectToCognito(accessToken) {
     AWS.config.region = 'us-east-1';
-    AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-        IdentityPoolId: 'us-east-1:456a4c88-7f96-4783-a82e-a97f2c9dc22a',
-        Logins: {
-            'graph.facebook.com': accessToken
-        }
-    });
+    AWS.config.update({
+        credentials: new AWS.CognitoIdentityCredentials({
+            IdentityPoolId: 'us-east-1:456a4c88-7f96-4783-a82e-a97f2c9dc22a',
+            Logins: {
+                'graph.facebook.com': accessToken
+            }
+        })
+    })
 }
